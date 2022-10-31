78ced3f75d32de5783e5f4af58e340594bd5a856
Qin Xu <qxuu0023@student.monash.edu>
2022-10-31 11:59:20 +1100

    created case_table funciton in output folder

---
+++ functions/output/case_Table.R
@@ -0,0 +1,75 @@
+# Case table function 
+
+case_Table <- function() {
+  # format function
+  formatFT <- function( inputFlextable ){
+    outputFlextable <- inputFlextable %>%
+      theme_zebra( odd_body='grey95' ) %>%
+      bg( bg='#091540', part='header' ) %>%
+      color( color='white', part='header' ) %>%
+      font( fontname='Arial', part='all' ) %>%
+      align( align="center", part="all") %>%
+      set_table_properties( layout="autofit")
+    return( outputFlextable )
+  }
+  
+  set_flextable_defaults( digits=2 )
+  
+  # constants
+  ageLevels <- factor( c('0-18', '19-39', '40-64', '65+'), ordered=TRUE )
+  
+  nAgeGroup <- length( levels( ageLevels ) )
+  
+  # connect to the board
+  board <- pins::board_register_rsconnect()
+  
+  # popAgeData
+  popData2020 <- pin_read( board=board
+                           ,name='craig.savage@health.vic.gov.au/pop2020')
+  popAgeData <- popData2020 %>%
+    mutate( AgeGroup=cut( age_abs
+                          , breaks=c(0,18,39,64,Inf)
+                          , labels=ageLevels
+                          , right=TRUE
+                          , include.lowest=TRUE
+                          , ordered_result=TRUE )
+    ) %>%
+    group_by( AgeGroup ) %>%
+    summarise( Pop=sum( population ))
+  
+  # read in case 
+  df <- pin_read(board,"qin.xu@health.vic.gov.au/epicases_linelist_read")
+  df<- df %>% mutate(YTD = as.logical(YTD),
+                     Thisweek = as.logical(Thisweek),
+                     Lastweek = as.logical(Lastweek))
+  
+  case_table<- df %>%
+    group_by(AgeGroup) %>%
+    summarise( YTD  = sum( YTD )
+               , Thisweek =sum( Thisweek),
+               Lastweek = sum( Lastweek)) %>%
+    inner_join( popAgeData, by='AgeGroup' ) %>%
+    adorn_totals() %>%
+    mutate(percentIncrease =((Thisweek -  Lastweek)/ Lastweek) %>%
+             percent( accuracy=0.1)
+           , ThisWeekPer100k=round(Thisweek /(Pop/100e3),1)
+           , ytdPer100k=round(YTD/(Pop/100e3),0)) 
+  
+  case_table <- case_table %>% 
+    select(c(AgeGroup,
+             YTD,
+             Thisweek,
+             Lastweek,
+             percentIncrease, 
+             ThisWeekPer100k, 
+             ytdPer100k))
+  
+  caseFT <- case_table %>%
+    flextable() %>%
+    formatFT()
+  
+  return(caseFT)
+  
+}
+
+case_Table() # time running 7secs
